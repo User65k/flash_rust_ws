45a2aa96dbaf8d31fb8c979ce3510b18ece6a205
User65k <15049544+User65k@users.noreply.github.com>
2023-02-26 13:38:00 +0100

    check symlinks in dav

--- src/dispatch/dav/mod.rs
+++ src/dispatch/dav/mod.rs
@@ -2,7 +2,10 @@ use crate::config::{AbsPathBuf, Utf8PathBuf};
 
 use super::staticf::{self, resolve_path, return_file, ResolveResult};
 use bytes::{BufMut, Bytes, BytesMut};
-use hyper::{body::HttpBody, header, Body, Request, Response, StatusCode};
+use hyper::{
+    body::HttpBody, header, http::HeaderValue, Body, HeaderMap, Method, Request, Response,
+    StatusCode,
+};
 use serde::Deserialize;
 use std::{
     convert::TryFrom,
@@ -12,15 +15,26 @@ use std::{
     pin::Pin,
     task::{Context, Poll},
 };
-use tokio::io::copy as redirect;
+use tokio::{fs::metadata, io::copy as redirect};
 use tokio::{
-    fs::{copy, create_dir, metadata, remove_dir_all, remove_file, rename, File},
+    fs::{copy, create_dir, remove_dir_all, remove_file, rename, File},
     io::AsyncRead,
 };
 mod propfind;
 use super::decode_and_normalize_path;
 #[cfg(test)]
 mod tests;
+
+#[allow(clippy::upper_case_acronyms)]
+enum DavMethod {
+    PROPFIND,
+    GET,
+    PUT,
+    COPY,
+    MOVE,
+    DELETE,
+    MKCOL,
+}
 
 /// req_path is relative from config.root
 /// web_mount is config.root from the client perspective
@@ -31,13 +45,8 @@ pub async fn do_dav(
     web_mount: &Utf8PathBuf,
     _remote_addr: SocketAddr,
 ) -> Result<Response<Body>, IoError> {
-    let abs_doc_root = config.dav.canonicalize()?;
-    let full_path = req_path.prefix_with(&abs_doc_root);
-    match req.method().as_ref() {
-        //strip root
-        //prepent mount
-        "PROPFIND" => propfind::handle_propfind(req, &full_path, abs_doc_root, web_mount).await,
-        "OPTIONS" => {
+    let m = match req.method() {
+        &Method::OPTIONS => {
             let rb = Response::builder()
                 .status(StatusCode::OK)
                 .header(
@@ -45,31 +54,95 @@ pub async fn do_dav(
                     "GET,PUT,OPTIONS,DELETE,PROPFIND,COPY,MOVE,MKCOL",
                 )
                 .header("DAV", "1");
-            Ok(rb.body(Body::empty()).unwrap())
+            return Ok(rb.body(Body::empty()).unwrap());
         }
-        "GET" => handle_get(req, &full_path, req_path, web_mount).await,
-        "PROPPATCH" => Err(IoError::new(
+        &Method::DELETE => DavMethod::DELETE,
+        &Method::GET => DavMethod::GET,
+        &Method::PUT => DavMethod::PUT,
+        m => match m.as_str() {
+            "PROPFIND" => DavMethod::PROPFIND,
+            "COPY" => DavMethod::COPY,
+            "MOVE" => DavMethod::MOVE,
+            "MKCOL" => DavMethod::MKCOL,
+            "PROPPATCH" => {
+                return Err(IoError::new(
+                    ErrorKind::PermissionDenied,
+                    "properties are read only",
+                ))
+            }
+            _ => {
+                return Ok(Response::builder()
+                    .status(StatusCode::METHOD_NOT_ALLOWED)
+                    .body(Body::empty())
+                    .expect("unable to build response"))
+            }
+        },
+    };
+    if config.read_only
+        && matches!(
+            m,
+            DavMethod::PUT
+                | DavMethod::COPY
+                | DavMethod::MOVE
+                | DavMethod::DELETE
+                | DavMethod::MKCOL
+        )
+    {
+        return Err(IoError::new(ErrorKind::PermissionDenied, "read only mount"));
+    }
+    let full_path = req_path.prefix_with(&config.dav);
+    let follow_symlinks = false;
+
+    if !follow_symlinks {
+        let fp = if matches!(m, DavMethod::PUT | DavMethod::MKCOL) {
+            //Path will be created -> check dad:
+            match full_path.parent() {
+                None => {
+                    return Err(IoError::new(
+                        ErrorKind::Other, //should not be reachable
+                        "No Parent",
+                    ));
+                }
+                Some(p) => match p.canonicalize() {
+                    Ok(c) => c,
+                    Err(ref e) if e.kind() == ErrorKind::NotFound => {
+                        let mut res = Response::new(Body::empty());
+                        *res.status_mut() = StatusCode::CONFLICT;
+                        return Ok(res);
+                    }
+                    Err(e) => return Err(e),
+                },
+            }
+        } else {
+            full_path.canonicalize()?
+        };
+        //check if the canonicalized version is still inside of the (abs) root path
+        if !fp.starts_with(&config.dav) {
+            return Err(IoError::new(
+                ErrorKind::PermissionDenied,
+                "Symlinks are not allowed",
+            ));
+        }
+    }
+
+    match m {
+        DavMethod::PROPFIND => {
+            propfind::handle_propfind(req, full_path, &config.dav, web_mount).await
+        }
+        DavMethod::GET => handle_get(req, &full_path, req_path, web_mount).await,
+        DavMethod::PUT => handle_put(req, &full_path, config.dont_overwrite).await,
+        DavMethod::MKCOL => handle_mkdir(&full_path).await,
+        DavMethod::COPY if !config.dont_overwrite => {
+            handle_copy(req.headers(), &full_path, &config.dav, web_mount).await
+        }
+        DavMethod::MOVE if !config.dont_overwrite => {
+            handle_move(req.headers(), &full_path, &config.dav, web_mount).await
+        }
+        DavMethod::DELETE if !config.dont_overwrite => handle_delete(&full_path).await,
+        _ => Err(IoError::new(
             ErrorKind::PermissionDenied,
             "properties are read only",
         )),
-        "PUT" if !config.read_only => handle_put(req, &full_path, config.dont_overwrite).await,
-        "COPY" if !config.read_only && !config.dont_overwrite => {
-            handle_copy(req, &full_path, &abs_doc_root, web_mount).await
-        }
-        "MOVE" if !config.read_only && !config.dont_overwrite => {
-            handle_move(req, &full_path, &abs_doc_root, web_mount).await
-        }
-        "DELETE" if !config.read_only && !config.dont_overwrite => {
-            handle_delete(req, &full_path).await
-        }
-        "MKCOL" if !config.read_only => handle_mkdir(req, &full_path).await,
-        "PUT" | "COPY" | "MOVE" | "DELETE" | "MKCOL" => {
-            Err(IoError::new(ErrorKind::PermissionDenied, "read only"))
-        }
-        _ => Ok(Response::builder()
-            .status(StatusCode::METHOD_NOT_ALLOWED)
-            .body(Body::empty())
-            .expect("unable to build response")),
     }
 }
 async fn list_dir(full_path: &Path, url_path: String) -> Result<Response<Body>, IoError> {
@@ -136,7 +209,7 @@ async fn handle_get(
         ResolveResult::Found(file, metadata, mime) => return_file(&req, file, metadata, mime).await,
     }
 }
-async fn handle_delete(_: Request<Body>, full_path: &Path) -> Result<Response<Body>, IoError> {
+async fn handle_delete(full_path: &Path) -> Result<Response<Body>, IoError> {
     let res = Response::new(Body::empty());
 
     let meta = metadata(full_path).await?;
@@ -149,20 +222,33 @@ async fn handle_delete(_: Request<Body>, full_path: &Path) -> Result<Response<Bo
     //HTTP NO_CONTENT ?
     Ok(res)
 }
-async fn handle_mkdir(_: Request<Body>, full_path: &Path) -> Result<Response<Body>, IoError> {
+async fn parent_exists(path: &Path) -> Result<bool, IoError> {
+    match path.parent() {
+        None => Err(IoError::new(
+            std::io::ErrorKind::PermissionDenied,
+            "No parent",
+        )),
+        Some(p) => match metadata(p).await {
+            Ok(m) => Ok(m.is_dir()),
+            Err(ref e) if e.kind() == ErrorKind::NotFound => Ok(false),
+            Err(e) => Err(e),
+        },
+    }
+}
+
+async fn handle_mkdir(full_path: &Path) -> Result<Response<Body>, IoError> {
     let mut res = Response::new(Body::empty());
-    match create_dir(full_path).await {
-        Ok(_) => *res.status_mut() = StatusCode::CREATED,
-        Err(ref e) if e.kind() == ErrorKind::NotFound => {
-            /*
-            A collection cannot be made at the Request-URI until
-            one or more intermediate collections have been created.  The server
-            MUST NOT create those intermediate collections automatically.
-            */
-            *res.status_mut() = StatusCode::CONFLICT;
-        }
-        Err(e) => return Err(e),
-    };
+    if !parent_exists(full_path).await? {
+        /*
+        A collection cannot be made at the Request-URI until
+        one or more intermediate collections have been created.  The server
+        MUST NOT create those intermediate collections automatically.
+        */
+        *res.status_mut() = StatusCode::CONFLICT;
+        return Ok(res);
+    }
+    create_dir(full_path).await?;
+    *res.status_mut() = StatusCode::CREATED;
     log::info!("Created {:?}", full_path);
     /*
     415 (Unsupported Media Type) - A body was sent
@@ -210,11 +296,17 @@ async fn handle_put(
     dont_overwrite: bool,
 ) -> Result<Response<Body>, IoError> {
     // Check if file exists before proceeding
-    if full_path.exists() && dont_overwrite {
+    if dont_overwrite && metadata(full_path).await.is_ok() {
         return Err(IoError::new(
             ErrorKind::AlreadyExists,
             "file overwriting is disabled",
         ));
+    }
+    if !parent_exists(full_path).await? {
+        //MUST 409 if folder does not exist
+        let mut res = Response::new(Body::empty());
+        *res.status_mut() = StatusCode::CONFLICT;
+        return Ok(res);
     }
     log::info!("about to store {:?}", full_path);
     let mut f = File::create(full_path).await?;
@@ -224,15 +316,17 @@ async fn handle_put(
     };
     redirect(&mut b, &mut f).await?;
     let res = Response::new(Body::empty());
-    //MUST 409 if folder does not exist
     //MAY 405 if file is a folder
     Ok(res)
 }
 /// get the absolute local destination dir from the request
-fn get_dst(req: &Request<Body>, root: &Path, web_mount: &Path) -> Result<PathBuf, IoError> {
+fn get_dst(
+    header: &HeaderMap<HeaderValue>,
+    root: &Path,
+    web_mount: &Path,
+) -> Result<PathBuf, IoError> {
     // Get the destination
-    let dst = req
-        .headers()
+    let dst = header
         .get("Destination")
         .map(|hv| hv.as_bytes())
         .and_then(|s| hyper::Uri::try_from(s).ok())
@@ -249,17 +343,22 @@ fn get_dst(req: &Request<Body>, root: &Path, web_mount: &Path) -> Result<PathBuf
     Ok(req_path)
 }
 async fn handle_copy(
-    req: Request<Body>,
+    header: &HeaderMap<HeaderValue>,
     src_path: &Path,
     root: &Path,
     web_mount: &Path,
 ) -> Result<Response<Body>, IoError> {
-    let dst_path = get_dst(&req, root, web_mount)?;
+    let dst_path = get_dst(header, root, web_mount)?;
     log::info!("Copy {:?} -> {:?}", src_path, dst_path);
     let mut res = Response::new(Body::empty());
+    if !parent_exists(&dst_path).await? {
+        //409 (Conflict) - No parent folder
+        *res.status_mut() = StatusCode::CONFLICT;
+        return Ok(res);
+    }
     //If a COPY request has an Overwrite header with a value of "F", and a resource exists at the Destination URL, the server MUST fail the request.
-    if let Some(b"F") = req.headers().get("Overwrite").map(|v| v.as_bytes()) {
-        if let Ok(_) = metadata(&dst_path).await {
+    if let Some(b"F") = header.get("Overwrite").map(|v| v.as_bytes()) {
+        if metadata(&dst_path).await.is_ok() {
             *res.status_mut() = StatusCode::PRECONDITION_FAILED;
             return Ok(res);
         }
@@ -282,8 +381,6 @@ async fn handle_copy(
     COPY could be that the source and destination resources are the same
     resource.
 
-    409 (Conflict) - No parent folder
-
     423 (Locked)
     502 (Bad Gateway)
     507 (Insufficient Storage)
@@ -291,24 +388,29 @@ async fn handle_copy(
     Ok(res)
 }
 async fn handle_move(
-    req: Request<Body>,
+    header: &HeaderMap<HeaderValue>,
     src_path: &Path,
     root: &Path,
     web_mount: &Path,
 ) -> Result<Response<Body>, IoError> {
-    let dst_path = get_dst(&req, root, web_mount)?;
+    let dst_path = get_dst(header, root, web_mount)?;
     log::info!("Move {:?} -> {:?}", src_path, dst_path);
     let mut res = Response::new(Body::empty());
-    match req.headers().get("Overwrite").map(|v| v.as_bytes()) {
+    if !parent_exists(&dst_path).await? {
+        //409 (Conflict) - No parent folder
+        *res.status_mut() = StatusCode::CONFLICT;
+        return Ok(res);
+    }
+    match header.get("Overwrite").map(|v| v.as_bytes()) {
         Some(b"F") => {
-            if let Ok(_) = metadata(&dst_path).await {
+            if metadata(&dst_path).await.is_ok() {
                 *res.status_mut() = StatusCode::PRECONDITION_FAILED;
                 return Ok(res);
             }
         }
         Some(b"T") => {
             //MUST perform a DELETE with "Depth: infinity" on the destination resource.
-            handle_delete(req, &dst_path).await?;
+            handle_delete(&dst_path).await?;
         }
         _ => {}
     }
@@ -319,7 +421,6 @@ async fn handle_move(
     204 (No Content) -> see copy
     207 (Multi-Status) -> see copy
     403 (Forbidden) -> see copy
-    409 (Conflict) -> see copy
     423 (Locked)
     502 (Bad Gateway)
     */
